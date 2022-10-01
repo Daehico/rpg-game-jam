a481f4e3729c007ff9ba022fe19a7bbfacb8e8bb
TSentner <centner_dc@mail.ru>
2022-10-01 17:55:30 +0500

    Add IUpgradable interface

--- Assets/Scripts/Upgrade/Upgrader.cs
+++ Assets/Scripts/Upgrade/Upgrader.cs
@@ -1,19 +1,47 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Upgrade
 {
-    public abstract class Upgrader : MonoBehaviour
+   
+    public interface IUpgradable
+    {
+        void Upgrade(int value);
+    } 
+    
+    public class Upgrader : MonoBehaviour
     {
         private int _level;
 
-        [Min(0f), SerializeField] private int _startValue, _increaseFactor;
+        [Min(0), SerializeField] private int _startValue, _increaseFactor;
+
+        [SerializeField] private MonoBehaviour _upgradableBehaviour;
+        private IUpgradable UpgradableInterface => 
+            (IUpgradable)_upgradableBehaviour;
 
         public event UnityAction OnUpgrade;
+        
+        private void OnValidate()
+        {
+            if (_upgradableBehaviour is IUpgradable == false)
+            {
+                Debug.LogError(_upgradableBehaviour.name + " needs to implement " + nameof(IUpgradable));
+                _upgradableBehaviour = null;
+            }
+        }
 
-        protected abstract void UpgradeHandler();
+        private void Awake()
+        {
+            UpgradeHandler();
+        }
 
-        protected int CalculateValue()
+        private void UpgradeHandler()
+        {
+            UpgradableInterface.Upgrade(CalculateValue()); 
+        }
+
+        private int CalculateValue()
         {
             return _startValue + _startValue * _level * _increaseFactor;
         }
@@ -22,6 +50,7 @@ namespace Upgrade
         {
             _level++;
             UpgradeHandler();
+            Debug.Log("Level = " + _level + " new value = " + CalculateValue());
             OnUpgrade?.Invoke();
         }
     }
